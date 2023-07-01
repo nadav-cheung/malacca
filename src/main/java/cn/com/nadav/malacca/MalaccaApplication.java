bc0b00d2169c8c4995b2018d6ed00f8a3b910f51
nadav <nadav.cheung@gmail.com>
2023-07-01 16:45:51 +0800

    first commit

---
+++ src/main/java/cn/com/nadav/malacca/MalaccaApplication.java
@@ -0,0 +1,13 @@
+package cn.com.nadav.malacca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalaccaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalaccaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/com/nadav/malacca/MalaccaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.nadav.malacca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalaccaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
